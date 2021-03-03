00697f117f6f09bf0650bafb217afe1f44fa004d
Ubuntu <ubuntu@ip-172-31-89-85.ec2.internal>
2021-03-03 21:31:26 +0000

    updating renew script

--- common/auto-ssl/auto-renew.sh
+++ common/auto-ssl/auto-renew.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-docker-compose -f $1 run certbot \
+echo $1
+
+docker-compose -f $1/docker-compose.yml run certbot \
 	renew --webroot \
 	--register-unsafely-without-email \
 	--agree-tos \
 	--no-random-sleep-on-renew \
 	--webroot-path=/data/letsencrypt >> ~/renew.log
 
-docker-compose -f $1 restart nginx
+docker-compose -f $1/docker-compose.yml restart nginx

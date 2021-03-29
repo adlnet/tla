70896b93ac6fd5e279d085f229a7d226eec82e94
TreyH <vbhayden3@gmail.com>
2021-03-29 16:41:01 -0400

    fixing import script

--- adl-xi/xi/import.js
+++ adl-xi/xi/import.js
@@ -14,7 +14,7 @@ let importFile = process.argv[2];
     
     let importPath = path.join(__dirname, importFile);
     let fileText = fs.readFileSync(importPath);
-    let fileData = JSON.stringify(fileText);
+    let fileData = JSON.parse(fileText);
     
     if (!Array.isArray(fileData)) {
         console.log("[XI Import]: (ERROR) Import format must be a JSON array.");
@@ -26,7 +26,8 @@ let importFile = process.argv[2];
     console.log(`[XI Import]: Importing ${fileData.length} records into XI ...`);
     console.log(`[XI Import]: ... record overwrite IS ${overwrite ? "NOT" : ""} enabled ...`);
 
-    let res = await mongo.post(fileData, overwrite).catch(console.error);
+    await mongo.init(null);
+    await mongo.post(fileData, overwrite).catch(console.error);
     
     console.log(`[XI Import]: Import Finished.`);
 

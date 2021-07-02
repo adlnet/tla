f38c39d1fa6000fcde4c0358355051f4966d5dcb
TreyH <vbhayden3@gmail.com>
2021-07-02 14:43:21 -0400

    trey updates

--- adl-portal/frontend/src/components/NavBar.jsx
+++ adl-portal/frontend/src/components/NavBar.jsx
@@ -14,28 +14,26 @@ export default class NavBar extends Component {
             <div>
                 <Navbar className="bg-color-nav py-3" variant="light" position="top" sticky="top">
                     <Navbar.Brand>
-                        <Link className="link-text" to='/'>
+                        <a class="link-text" target="_blank" href='https://adlnet.gov'>
                             <img
                                 src={logo}
                                 width="45"
                                 height="30"
                                 alt="ADL Initiative Logo"
                             />
-                        </Link>
+                        </a>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link>
-                                <Link className="link-text text-color-nav" to="/">
-                                    Home
-                                </Link>
+                            <Nav.Link className="link-text text-color-nav" to="/">
+                                Home
                             </Nav.Link>
-                            <Nav.Link>
+                            {/* <Nav.Link>
                                 <Link className="link-text text-color-nav" to="/visualizer">
                                     Visualizer
                                 </Link>
-                            </Nav.Link>
+                            </Nav.Link> */}
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>

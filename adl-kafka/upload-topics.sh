6a3b64ff533bfb49f293c7046916894539f614a7
Trey Hayden <33755714+vbhayden@users.noreply.github.com>
2022-02-09 14:36:28 -0500

    Update upload-topics.sh

--- adl-kafka/upload-topics.sh
+++ adl-kafka/upload-topics.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker exec kafka_1 mkdir /tmp/kafka
+
 docker cp ./kafka/create-topics.sh kafka_1:/tmp/kafka/create-topics.sh
 docker cp ./topics.txt kafka_1:/tmp/kafka/tla-topics.txt
 
